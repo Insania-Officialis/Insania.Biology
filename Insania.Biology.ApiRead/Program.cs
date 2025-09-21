97c4b2c0855d216e1ac9392bba7768c75de89834
Альтаир фон Альфхейм <insania_officialis@outlook.com>
2025-09-21 14:51:44 +0300

    - Доработки начального запуска проекта - Добавление кода стауса в логи - Подключение конвейера авторизации - Исправление ошибки с записью токена в логи - Исправление ошибки с корсами - Исправление ошибки с двойной сериализации тела запроса и ответа в логах

--- Insania.Biology.ApiRead/Program.cs
+++ Insania.Biology.ApiRead/Program.cs
@@ -137,14 +137,12 @@ services.AddSwaggerGen(options =>
 //Добавление корсов
 services.AddCors(options =>
 {
-    options.AddPolicy("BadPolicy", policyBuilder => policyBuilder
+    options.AddPolicy("CorsPolicy", policyBuilder => policyBuilder
         .SetIsOriginAllowed(origin => true)
         .AllowAnyMethod()
         .AllowAnyHeader()
         .AllowCredentials()
     );
-
-    options.DefaultPolicyName = "BadPolicy";
 });
 
 //Добавление контроллеров
@@ -161,14 +159,20 @@ services
 //Добавление параметров преобразования моделей
 services.AddAutoMapper(typeof(BiologyMappingProfile));
 
+//Регистрация списка исключений авторизации
+builder.Services.AddSingleton<List<string>>(
+[
+
+]);
+
 //Построение веб-приложения
 WebApplication app = builder.Build();
 
-//Добавление параметров конвейера запросов
-app.UseMiddleware<LoggingMiddleware>();
-
 //Подключение маршрутизации
 app.UseRouting();
+
+//Подключение корсов
+app.UseCors("CorsPolicy");
 
 //Подключение аутентификации
 app.UseAuthentication();
@@ -176,15 +180,16 @@ app.UseAuthentication();
 //Подключение авторизации
 app.UseAuthorization();
 
+//Добавление параметров конвейера запросов
+app.UseMiddleware<LoggingMiddleware>(); //логгирование
+app.UseMiddleware<Insania.Shared.Middleware.AuthorizationMiddleware>(); //авторизация
+
 //Подключение сваггера
 app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "Insania API V1");
 });
-
-//Подключение корсов
-app.UseCors();
 
 //Подключение маршрутизации контроллеров
 app.MapControllers();
